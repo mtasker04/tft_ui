71679fd66c046597278e597b64b008d0182bedc1
Max Tasker <162042155+mtasker04@users.noreply.github.com>
2024-12-19 15:06:59 +0000

    update tft_ui.h

--- src/TFT_UI.h
+++ src/TFT_UI.h
@@ -1,12 +1,41 @@
 #pragma once
 
 //#include <TFT_eSPI.h>
+class TFT_eSPI;
+class TFT_eSPRITE;
+
+#define TFT_UI_DOUBLE_BUFFER
+
+// double buffering
+#ifdef TFT_UI_DOUBLE_BUFFER
+  typedef TFT_eSPRITE tft_ui_buf_t;
+#endif
+
+
+// foreward declare classes
+class TFT_UI;
 
 // TFT_UI object
 class TFT_UI {
 public:
+    // constructor
+    // @param pTFT: pointer to TFT_eSPI object
     inline TFT_UI(TFT_eSPI* pTFT);
+
+// drawing utility functions
+public:
+    // begin drawing to the back buffer
+    inline void begin();
+    // finish drawing and swap buffers
+    inline void end();
+    // get the back buffer
+    inline tft_ui_buf_t* getBackBuffer();
+
+// drawing functions
+public:
+    
 
 private:
     TFT_eSPI* tft;
-}
+    tft_ui_buf_t buf[2];
+};
